e2a7d1eccaccbc205f1b33cc332ebdaa3dc96807
Sean Coleman <bramblestar1.0@icloud.com>
2026-07-30 15:41:32 -0700

    add preprocessing

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
 import numpy as np
 import pandas as pd
 import gymnasium as gym
+import torch
+from torch import nn
 
+replay_memory = pd.DataFrame(columns=["state", "action", "reward", "next_state", "done"])
+
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+torch.set_default_device(device=device)
+print(f"Using {device} device")
+
+# class Q_Value_Function(nn.Module): #use more low-level solution instead of this
+#     def __init__(self):
+#         super.__init__()
+#         self.flatten = nn.Flatten()
+
+def preprocessing(obs):
+    #use ITU-R 601-2 luma formula to convert to grayscale
+    obs = np.dot(obs[..., :3], [0.299, 0.587, 0.114])
+    obs = obs.flatten()
+    return obs
+
+        
+
+env = gym.make("CarRacing-v3", render_mode="human", lap_complete_percent=0.95, domain_randomize=False, continuous=True, max_episode_steps=1000,)
+
+obs, _ = env.reset()
+
+print(obs.shape)
+
+obs = preprocessing(obs)
+
+print(obs.shape)
 
 
